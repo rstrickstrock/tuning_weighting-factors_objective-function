347ac1b20f3a51329ce4b01115da7af90a28221c
rstric2s <robin.strickstrock@smail.emt.h-brs.de>
2024-01-15 16:41:14 +0100

    added lin_init

---
+++ input_data/wf_lin_init/batch_run_PP-25_MM-75.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --partition=hpc3
+#SBATCH --nodes=1
+#SBATCH --mem 1G
+#SBATCH --time=72:00:00
+#SBATCH --job-name=lPP25MM75
+
+./run_PP-25_MM-75.sh
---
+++ input_data/wf_lin_init/batch_run_PP-80_MM-20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --partition=hpc3
+#SBATCH --nodes=1
+#SBATCH --mem 1G
+#SBATCH --time=72:00:00
+#SBATCH --job-name=lPP80MM20
+
+./run_PP-80_MM-20.sh
