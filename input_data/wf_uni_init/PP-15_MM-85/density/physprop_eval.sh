5f907a156401a1d25142f6ec4b79a27547d3693d
rstric2s <robin.strickstrock@smail.emt.h-brs.de>
2024-01-15 16:44:25 +0100

    added uni_init

---
+++ input_data/wf_uni_init/PP-15_MM-85/density/physprop_eval.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+eval_cwd=$1
+#echo "eval_cwd: $eval_cwd"
+
+cd $eval_cwd
+sbatch batch_slurm_eval_PhysProp.sh
---
+++ input_data/wf_uni_init/batch_run_PP-50_MM-50.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --partition=hpc3
+#SBATCH --nodes=1
+#SBATCH --mem 1G
+#SBATCH --time=72:00:00
+#SBATCH --job-name=uPP50MM50
+
+./run_PP-50_MM-50.sh
